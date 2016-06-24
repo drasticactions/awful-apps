dab66117c884366b911328f8e963d88070c4a19b
Tim Miller <innerlogic4321@gmail.com>
2016-06-23 21:06:52 -0400

    Add Markov Chain app and post collector app

---
+++ sa-markov-bot/sa-markov-bot/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MarkovSharp.TokenisationStrategies;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace sa_markov_bot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           MainAsync(args).GetAwaiter().GetResult();
+        }
+
+        static async Task MainAsync(string[] args)
+        {
+            var path = args.FirstOrDefault();
+            if (string.IsNullOrEmpty(path))
+            {
+                Console.WriteLine("Enter File Path");
+                path = Console.ReadLine();
+            }
+            var lines = File.ReadAllLines(path);
+            var markov = new StringMarkov();
+            markov.Learn(lines);
+            var newLines = markov.Walk(10).ToList();
+            for (var i = 0; i < newLines.Count(); i++)
+            {
+                Console.WriteLine(newLines[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ sa-post-collector/sa-post-collector/Program.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AwfulRedux.Core.Managers;
+using AwfulRedux.Core.Models.Posts;
+using Newtonsoft.Json;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace sa_post_collector
+{
+    class Program
+    {
+        static void Main()
+        {
+            MainAsync().GetAwaiter().GetResult();
+        }
+
+        static async Task MainAsync()
+        {
+            Console.WriteLine("Enter your Something Awful Username: ");
+            var username = Console.ReadLine();
+            Console.WriteLine("Enter your SA Password: ");
+            var password = ReadPassword();
+            Console.WriteLine("Logging in...");
+            var authenticationManager = new AuthenticationManager();
+            var result = await authenticationManager.AuthenticateAsync(username, password);
+            if (!result.IsSuccess)
+            {
+                Console.WriteLine($"Error logging in: {result.Error}");
+                return;
+            }
+
+            var webManager = new WebManager(result.AuthenticationCookie);
+            var postManager = new PostManager(webManager);
+
+            Console.WriteLine("Enter the SA Thread ID: ");
+            var threadidstring = Console.ReadLine();
+            long threadId = 0;
+            try
+            {
+                threadId = Convert.ToInt64(threadidstring);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Error: Bad Thread ID");
+                return;
+            }
+
+            Console.WriteLine("Enter the SA User ID: ");
+            var useridstring = Console.ReadLine();
+            int userid = 0;
+            try
+            {
+                userid = Convert.ToInt32(useridstring);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Error: Bad Thread ID");
+                return;
+            }
+
+            // TODO: Make a real "GetThreadInfo" method, this is _lame_.
+            var threadInfo =
+                await
+                    postManager.GetUsersPostsInThreadAsync(
+                        $"https://forums.somethingawful.com/showthread.php?threadid={threadId}", userid, 1, false, false, true);
+            var posts = JsonConvert.DeserializeObject<ThreadPosts>(threadInfo.ResultJson);
+            var postername = posts.Posts.FirstOrDefault()?.User.Username;
+            var totalPages = posts.ForumThread.TotalPages;
+            Console.WriteLine($"Getting posts for {postername} in {posts.ForumThread.Name}. Total Pages: {totalPages}");
+            for (var i = 1; i <= totalPages; i++)
+            {
+                Console.WriteLine($"Loading page {i}");
+                var postsJson2 =
+                await
+                    postManager.GetUsersPostsInThreadAsync(
+                        $"https://forums.somethingawful.com/showthread.php?threadid={threadId}", userid, i, false, false, true);
+                var posts2 = JsonConvert.DeserializeObject<ThreadPosts>(postsJson2.ResultJson);
+                var postElements = posts2.Posts.Select(node => node.PostElements);
+                var innerTextPosts = postElements.Select(node => node.InnerText);
+                File.AppendAllLines($"{postername} - {Regex.Replace(posts.ForumThread.Name, "[" + Regex.Escape(new string(Path.GetInvalidFileNameChars())) + "]", "_")}.txt", innerTextPosts);
+            }
+        }
+
+        // From http://stackoverflow.com/questions/29201697/hide-replace-when-typing-a-password-c
+        static string ReadPassword()
+        {
+            string password = "";
+            ConsoleKeyInfo info = Console.ReadKey(true);
+            while (info.Key != ConsoleKey.Enter)
+            {
+                if (info.Key != ConsoleKey.Backspace)
+                {
+                    Console.Write("*");
+                    password += info.KeyChar;
+                }
+                else if (info.Key == ConsoleKey.Backspace)
+                {
+                    if (!string.IsNullOrEmpty(password))
+                    {
+                        // remove one character from the list of password characters
+                        password = password.Substring(0, password.Length - 1);
+                        // get the location of the cursor
+                        int pos = Console.CursorLeft;
+                        // move the cursor to the left by one character
+                        Console.SetCursorPosition(pos - 1, Console.CursorTop);
+                        // replace it with space
+                        Console.Write(" ");
+                        // move the cursor to the left by one character again
+                        Console.SetCursorPosition(pos - 1, Console.CursorTop);
+                    }
+                }
+                info = Console.ReadKey(true);
+            }
+            // add a new line because user pressed enter at the end of their password
+            Console.WriteLine();
+            return password;
+        }
+    }
+}
